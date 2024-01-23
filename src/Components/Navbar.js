1af42d86b8bd61ad54949e27e395f8a98f7991ae
Maruf77 <marufkhan2002@gmail.com>
2024-01-23 19:42:52 +0530

    fix-build

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,44 +6,47 @@ const Navbar = () => {
     <section className="relative mx-auto">
       <nav className="flex justify-between bg-gray-900 text-white w-full">
         <div className="px-5 xl:px-12 py-3 flex w-full items-center">
-      <div className="flex mt-4">
-          <a className="text-3xl font-bold font-heading" >
-            <img className="h-12 m-auto" src={Icon} alt="logo" />
-          </a>
+          <div className="flex mt-4">
+            <a href="/" className="text-3xl font-bold font-heading">
+              {/* Provide a valid href attribute */}
+              <img className="h-12 m-auto" src={Icon} alt="logo" />
+            </a>
           </div>
           <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
             <li>
-              <a className="hover:text-gray-200" >
+              <a href="/" className="hover:text-gray-200">
                 Home
               </a>
             </li>
             <li>
-              <a className="hover:text-gray-200" >
+              <a href="/" className="hover:text-gray-200">
                 Services
               </a>
             </li>
             <li>
-              <a className="hover:text-gray-200" >
+              <a href="/" className="hover:text-gray-200">
                 Blogs
               </a>
             </li>
             <li>
-              <a className="hover:text-gray-200" >
+              <a href="/" className="hover:text-gray-200">
                 About
               </a>
             </li>
           </ul>
-          
+
           {/* Header Icons */}
           <div className="hidden xl:flex items-center space-x-5">
-            <button className="md:ml-auto bg-[#2f93a0] hover:bg-[#49becd] text-white font-bold py-2 px-4 rounded-full">
+            <a href="/" className="md:ml-auto bg-[#2f93a0] hover:bg-[#49becd] text-white font-bold py-2 px-4 rounded-full">
+              {/* Provide a valid href attribute */}
               Book Now
-            </button>
+            </a>
           </div>
         </div>
 
         {/* Responsive navbar */}
-        <a className="navbar-burger self-center mr-12 xl:hidden" >
+        <a href="/" className="navbar-burger self-center mr-12 xl:hidden">
+          {/* Provide a valid href attribute */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 hover:text-gray-200"
@@ -52,9 +55,9 @@ const Navbar = () => {
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
